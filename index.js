267a08d32cd99d9cc8fed31afdfd16ddcbbdec56
anush <anush@10.200.99.30>
2017-05-11 14:44:58 -0700

    Add configuration

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-const request = require('request');
 const rp = require('request-promise-native');
 
 class TPClient {
 	constructor(credentials, ip) {
 		this._credentials = credentials;
 		this._ip = ip;
-        this._base_url = `http://${ip}`;
+		this._baseUrl = `http://${ip}`;
 	}
 
 	get credentials() {
 		return this._credentials;
 	}
 
-    get ip() {
-        return this._ip;
-    }
+	get ip() {
+		return this._ip;
+	}
 
-    buildOptions(method, endpoint) {
-        const options = {
-            auth: {
-                username: this._credentials.username,
-                password: this._credentials.password
-            },
-            method,
-            uri: `${this._base_url}/${endpoint}`,
-            headers: {
-                'Content-Type': 'application/xml',
-                
-            }
-        };
+	buildOptions(method, endpoint) {
+		const options = {
+			auth: {
+				username: this._credentials.username,
+				password: this._credentials.password
+			},
+			method,
+			uri: `${this._baseUrl}/${endpoint}`,
+			headers: {
+				'Content-Type': 'application/xml'
+			}
+		};
 
-        return options;
-    }
+		return options;
+	}
 
-    
+	getConfiguration() {
+		const options = this.buildOptions('GET', 'configuration.xml');
+		return rp.get(options);
+	}
+
 	getCommands() {
-        const options = this.buildOptions('GET', 'command.xml');
-        return rp.get(options);
+		const options = this.buildOptions('GET', 'command.xml');
+		return rp.get(options);
 	}
 }
-
 
 module.exports = TPClient;
--- test.js
+++ test.js
@@ -16,33 +16,42 @@ test('Set credentials', t => {
 });
 
 test('Set address', t => {
-    t.is(client.ip, '192.168.0.1');
+	t.is(client.ip, '192.168.0.1');
 });
 
 test('Build params', t => {
-    const actualOptions = client.buildOptions('IP', 'endpoint');
-    const expectedOptions = {
-        auth:{
-            username: 'username',
-            password: 'password'
-        },
-        method: 'IP',
-        uri: 'http://192.168.0.1/endpoint',
-        headers: {
-            'Content-Type': 'application/xml'
-        }
-    };
+	const actualOptions = client.buildOptions('IP', 'endpoint');
+	const expectedOptions = {
+		auth: {
+			username: 'username',
+			password: 'password'
+		},
+		method: 'IP',
+		uri: 'http://192.168.0.1/endpoint',
+		headers: {
+			'Content-Type': 'application/xml'
+		}
+	};
 
-    t.deepEqual(actualOptions, expectedOptions);
-
+	t.deepEqual(actualOptions, expectedOptions);
 });
 
 test('Get command', t => {
-    BASE_NOCK
-        .get('/command.xml')
-        .reply(200, `<command></command>`);
-    
+	BASE_NOCK
+		.get('/command.xml')
+		.reply(200, `<command></command>`);
+
 	client
-        .getCommands()
-        .then(resp => t.is(resp, '<command></command>'));
+		.getCommands()
+		.then(resp => t.is(resp, '<command></command>'));
+});
+
+test('Get configuration', t => {
+	BASE_NOCK
+		.get('/configuration.xml')
+		.reply(200, `<configuration></configuration>`);
+
+	client
+		.getConfiguration()
+		.then(resp => t.is(resp, '<configuration></configuration>'));
 });
