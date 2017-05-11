2a8424c3b840b87cb4121efe5da38c39734a503d
Brandon Him <brh55@drexel.edu>
2017-05-11 12:45:47 -0700

    Add buildOptions and command endpoint

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
+const request = require('request');
+const rp = require('request-promise-native');
+
 class TPClient {
-	constructor(credentials) {
-		this._credentials = credentials || {};
+	constructor(credentials, ip) {
+		this._credentials = credentials;
+		this._ip = ip;
+        this._base_url = `http://${ip}`;
 	}
 
 	get credentials() {
 		return this._credentials;
 	}
+
+    get ip() {
+        return this._ip;
+    }
+
+    buildOptions(method, endpoint) {
+        const options = {
+            auth: {
+                username: this._credentials.username,
+                password: this._credentials.password
+            },
+            method,
+            uri: `${this._base_url}/${endpoint}`,
+            headers: {
+                'Content-Type': 'application/xml',
+                
+            }
+        };
+
+        return options;
+    }
+
+    
+	getCommands() {
+        const options = this.buildOptions('GET', 'command.xml');
+        return rp.get(options);
+	}
 }
+
 
 module.exports = TPClient;
--- test.js
+++ test.js
@@ -1,14 +1,48 @@
 import test from 'ava';
+import nock from 'nock';
 import CiscoTPClient from '.';
 
-test('Set credentials', t => {
-	const client = new CiscoTPClient({
-		username: 'username',
-		password: 'password'
-	});
+const IP = '192.168.0.1';
+const BASE_NOCK = nock(`http://${IP}`);
 
-	console.log(client);
+const client = new CiscoTPClient({
+	username: 'username',
+	password: 'password'
+}, IP);
+
+test('Set credentials', t => {
 	t.is(client.credentials.username, 'username');
 	t.is(client.credentials.password, 'password');
 });
 
+test('Set address', t => {
+    t.is(client.ip, '192.168.0.1');
+});
+
+test('Build params', t => {
+    const actualOptions = client.buildOptions('IP', 'endpoint');
+    const expectedOptions = {
+        auth:{
+            username: 'username',
+            password: 'password'
+        },
+        method: 'IP',
+        uri: 'http://192.168.0.1/endpoint',
+        headers: {
+            'Content-Type': 'application/xml'
+        }
+    };
+
+    t.deepEqual(actualOptions, expectedOptions);
+
+});
+
+test('Get command', t => {
+    BASE_NOCK
+        .get('/command.xml')
+        .reply(200, `<command></command>`);
+    
+	client
+        .getCommands()
+        .then(resp => t.is(resp, '<command></command>'));
+});
