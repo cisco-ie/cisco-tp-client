e0df214936d07bbe28ab2175ca7303411b224320
Brandon Him <brh55@drexel.edu>
2017-05-12 15:29:42 -0700

    Remove fun.js

--- fun.js
+++
@@ -1,8 +0,0 @@
-const CiscoTPClient = require('.');
-
-const TP = new CiscoTPClient({
-	username: 'mllewell',
-	password: 'Cisco123!'
-}, '10.88.56.186');
-
-TP.putXml('/Status/Audio').then(response => console.log(response));
